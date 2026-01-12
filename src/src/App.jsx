ff4bafaef1dd9aa710b46d3cf167da6fb19e069f
8Mileuzed <8mileuzed@gmail.com>
2026-01-12 10:52:17 -0500

    Delete src/src/App.jsx

--- src/src/App.jsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ padding: "40px", fontFamily: "Arial" }}>
-      <h1>8 Mile Uzed Autoz</h1>
-      <p>Quality Used Cars • Financing Available</p>
-    </div>
-  );
-}
