6e041a2a2cfb6d74222383d08b49366c7d20c16d
8Mileuzed <8mileuzed@gmail.com>
2026-01-12 11:45:20 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App"; // DO NOT add .jsx, must match the file name exactly
+import App from "./App";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
