302040ca8bc399b80ea01a02d5a87d5d068fb5d0
8Mileuzed <8mileuzed@gmail.com>
2026-01-12 11:33:24 -0500

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,11 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header style={{ background: "#0b3d91", color: "#fff", padding: "20px", textAlign: "center" }}>
+      <h1>8 Mile Uzed Autoz</h1>
+      <p>Quality Used Cars • Financing Available • Warren, MI</p>
+    </header>
+  );
+}
 
