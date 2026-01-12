8292744a7e05314b7dcdd6f2dde755a741b00128
8Mileuzed <8mileuzed@gmail.com>
2026-01-12 11:02:04 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,11 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  root: ".", // root is repo root
+  build: {
+    outDir: "dist", // output folder for Vercel
+  },
+});
