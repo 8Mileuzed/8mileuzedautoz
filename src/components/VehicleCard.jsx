f738cbf7db21d7c3e422364bb6c45cb487552ba9
8Mileuzed <8mileuzed@gmail.com>
2026-01-12 11:34:16 -0500

    Update VehicleCard.jsx

--- src/components/VehicleCard.jsx
+++ src/components/VehicleCard.jsx
@@ -1,1 +1,13 @@
+import React from "react";
+
+export default function VehicleCard({ vehicle }) {
+  return (
+    <div style={{ border: "1px solid #ccc", borderRadius: "8px", margin: "10px", padding: "10px", maxWidth: "300px" }}>
+      <img src={vehicle.image} alt={vehicle.name} style={{ width: "100%", borderRadius: "8px" }} />
+      <h3>{vehicle.name}</h3>
+      <p>Mileage: {vehicle.mileage}</p>
+      <p>Price: ${vehicle.price}</p>
+    </div>
+  );
+}
 
